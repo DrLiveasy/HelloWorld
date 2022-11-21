0b9fdc2e31723f2b4080806ee088b3c782b40623
DrLiveasy <youssef.saleh.hans@gmail.com>
2022-11-21 11:51:51 +0100

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("ciao come va?");
+
+var risposta = Console.ReadLine();
+
+Console.WriteLine("la risposta inserita dall'utente " + risposta);
